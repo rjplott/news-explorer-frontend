d48bf0cae22e578cc595c34bb89dbac6c46ddd57
Joe Lott <rjplott@gmail.com>
2022-03-03 18:15:21 -0500

    feat: convert NewsCard component to typescript

--- src/components/NewsCard/NewsCard.tsx
+++ src/components/NewsCard/NewsCard.tsx
@@ -1,6 +1,15 @@
 import './NewsCard.css';
 import { useHistory } from 'react-router-dom';
+import * as React from 'react';
 import { useState } from 'react';
+
+type Props = {
+  isLoggedIn: boolean;
+  card: Card;
+  handleSaveCard: () => void;
+  handleUnsaveCard: () => void;
+  handleOpenRegister: () => void;
+};
 
 export default function NewsCard({
   isLoggedIn,
@@ -8,7 +17,7 @@ export default function NewsCard({
   handleSaveCard,
   handleUnsaveCard,
   handleOpenRegister,
-}) {
+}: Props) {
   const path = useHistory().location.pathname;
   const [isHovering, setIsHovering] = useState(false);
   const [isBookmarked, setIsBookmarked] = useState(false);
@@ -21,52 +30,52 @@ export default function NewsCard({
   );
 
   const bookmark = (
-    <svg width="24" height="24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <svg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'>
       <path
-        d="M11.382 15.714 6 19.942V4h12v15.942l-5.382-4.228-.618-.486-.618.486Z"
-        stroke="#B6BCBF"
-        strokeWidth="2"
+        d='M11.382 15.714 6 19.942V4h12v15.942l-5.382-4.228-.618-.486-.618.486Z'
+        stroke='#B6BCBF'
+        strokeWidth='2'
       />
     </svg>
   );
 
   const hoverBookermark = (
-    <svg width="24" height="24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <svg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'>
       <path
-        d="M11.382 15.714 6 19.942V4h12v15.942l-5.382-4.228-.618-.486-.618.486Z"
-        stroke="#1A1B22"
-        strokeWidth="2"
+        d='M11.382 15.714 6 19.942V4h12v15.942l-5.382-4.228-.618-.486-.618.486Z'
+        stroke='#1A1B22'
+        strokeWidth='2'
       />
     </svg>
   );
 
   const savedBookmark = (
-    <svg width="24" height="24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <svg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'>
       <path
-        d="M5 4a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v18l-7-5.5L5 22V4Z"
-        fill="#2F71E5"
+        d='M5 4a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v18l-7-5.5L5 22V4Z'
+        fill='#2F71E5'
       />
     </svg>
   );
 
   const trashIcon = (
-    <svg width="24" height="24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <svg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'>
       <path
-        fillRule="evenodd"
-        clipRule="evenodd"
-        d="M15 3H9v2H3v2h18V5h-6V3ZM5 9v11a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V9h-2v11H7V9H5Zm4 0v9h2V9H9Zm4 0v9h2V9h-2Z"
-        fill="#B6BCBF"
+        fillRule='evenodd'
+        clipRule='evenodd'
+        d='M15 3H9v2H3v2h18V5h-6V3ZM5 9v11a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V9h-2v11H7V9H5Zm4 0v9h2V9H9Zm4 0v9h2V9h-2Z'
+        fill='#B6BCBF'
       />
     </svg>
   );
 
   const hoverTrashIcon = (
-    <svg width="24" height="24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <svg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'>
       <path
-        fillRule="evenodd"
-        clipRule="evenodd"
-        d="M15 3H9v2H3v2h18V5h-6V3ZM5 9v11a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V9h-2v11H7V9H5Zm4 0v9h2V9H9Zm4 0v9h2V9h-2Z"
-        fill="#1A1B22"
+        fillRule='evenodd'
+        clipRule='evenodd'
+        d='M15 3H9v2H3v2h18V5h-6V3ZM5 9v11a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V9h-2v11H7V9H5Zm4 0v9h2V9H9Zm4 0v9h2V9h-2Z'
+        fill='#1A1B22'
       />
     </svg>
   );
@@ -95,7 +104,7 @@ export default function NewsCard({
 
   if (path === '/') {
     return (
-      <article className="news-card">
+      <article className='news-card'>
         {!isLoggedIn ? (
           <div
             className={`news-card__tooltip ${
@@ -116,9 +125,9 @@ export default function NewsCard({
           ''
         )}
         <button
-          className="news-card__save-button news-card__button"
-          type="button"
-          aria-label="Save Article"
+          className='news-card__save-button news-card__button'
+          type='button'
+          aria-label='Save Article'
           onMouseEnter={handleHover}
           onMouseLeave={handleHover}
           onClick={handleSaveClick}
@@ -129,18 +138,18 @@ export default function NewsCard({
             ? hoverBookermark
             : bookmark}
         </button>
-        <img className="news-card__image" src={card.image} alt="" />
-        <p className="news-card__date">{convertedDate}</p>
-        <h2 className="news-card__title">{card.title}</h2>
-        <p className="news-card__content">{card.text}</p>
-        <p className="news-card__source">{card.source}</p>
+        <img className='news-card__image' src={card.image} alt='' />
+        <p className='news-card__date'>{convertedDate}</p>
+        <h2 className='news-card__title'>{card.title}</h2>
+        <p className='news-card__content'>{card.text}</p>
+        <p className='news-card__source'>{card.source}</p>
       </article>
     );
   }
 
   return (
-    <article className="news-card">
-      <div className="news-card__tag">{card.keyword}</div>
+    <article className='news-card'>
+      <div className='news-card__tag'>{card.keyword}</div>
       <div
         className={`news-card__tooltip ${
           isHovering && 'news-card__tooltip_visible'
@@ -149,20 +158,20 @@ export default function NewsCard({
         Remove from saved
       </div>
       <button
-        className="news-card__delete-button news-card__button"
-        type="button"
-        aria-label="Delete Article"
+        className='news-card__delete-button news-card__button'
+        type='button'
+        aria-label='Delete Article'
         onMouseEnter={handleHover}
         onMouseLeave={handleHover}
         onClick={handleDeleteClick}
       >
         {isHovering ? hoverTrashIcon : trashIcon}
       </button>
-      <img className="news-card__image" src={card.image} alt="" />
-      <p className="news-card__date">{convertedDate}</p>
-      <h2 className="news-card__title">{card.title}</h2>
-      <p className="news-card__content">{card.text}</p>
-      <p className="news-card__source">{card.source}</p>
+      <img className='news-card__image' src={card.image} alt='' />
+      <p className='news-card__date'>{convertedDate}</p>
+      <h2 className='news-card__title'>{card.title}</h2>
+      <p className='news-card__content'>{card.text}</p>
+      <p className='news-card__source'>{card.source}</p>
     </article>
   );
 }
