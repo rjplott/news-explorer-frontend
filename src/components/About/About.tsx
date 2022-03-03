54919e22b7a6ee5b157c30800e16796ffd78e137
Joe Lott <rjplott@gmail.com>
2022-03-03 17:30:11 -0500

    feat: convert About component to typescript

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -4,15 +4,15 @@ import './About.css';
 
 export default function About() {
   return (
-    <section className="about">
+    <section className='about'>
       <img
-        alt="A woman sitting on some grass meditating with her legs crossed"
+        alt='A woman sitting on some grass meditating with her legs crossed'
         src={avatar}
-        className="about__image"
+        className='about__image'
       />
-      <div className="about__text-wrapper">
-        <h2 className="about__title">About the author</h2>
-        <p className="about__text">
+      <div className='about__text-wrapper'>
+        <h2 className='about__title'>About the author</h2>
+        <p className='about__text'>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
           facilisis sagittis mollis. Nulla condimentum elit sapien, eget iaculis
           odio mattis sit amet. Curabitur eu tellus libero. Aliquam bibendum leo
