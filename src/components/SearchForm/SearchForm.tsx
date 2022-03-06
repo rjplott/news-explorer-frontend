7dc71cd7310815aca8c5342980432b4a08d6f277
Joe Lott <rjplott@gmail.com>
2022-03-06 09:45:12 -0500

    feat: convert SeachForm component to typescript

--- src/components/SearchForm/SearchForm.tsx
+++ src/components/SearchForm/SearchForm.tsx
@@ -1,59 +1,64 @@
 import './SearchForm.css';
 import Header from '../Header/Header';
-import { useState } from 'react';
+import React, { useState } from 'react';
+
+interface Props {
+  isLoggedIn: boolean;
+  onSignInClick: () => void;
+  handleLogout: () => void;
+  handleRequestNews: (str: string) => void;
+}
 
 export default function SearchForm({
   isLoggedIn,
   onSignInClick,
   handleLogout,
-  name,
   handleRequestNews,
-}) {
+}: Props): JSX.Element {
   const [searchValue, setSearchValue] = useState('');
   const [canSearch, setCanSearch] = useState(false);
 
-  const handleSubmit = (e) => {
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>): void => {
     e.preventDefault();
     handleRequestNews(searchValue);
     setSearchValue('');
     setCanSearch(false);
   };
 
-  const handleInputChange = (e) => {
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
     setSearchValue(e.target.value);
     setCanSearch(e.target.validity.valid);
   };
 
   return (
-    <div className="news-search">
+    <div className='news-search'>
       <Header
         isLoggedIn={isLoggedIn}
         onSignInClick={onSignInClick}
         handleLogout={handleLogout}
-        name={name}
       />
-      <form className="seach-form" onSubmit={handleSubmit}>
-        <h2 className="search-form__title">What's going on in the world?</h2>
-        <p className="search-form__subtitle">
+      <form className='seach-form' onSubmit={handleSubmit}>
+        <h2 className='search-form__title'>What's going on in the world?</h2>
+        <p className='search-form__subtitle'>
           Find the latest news on any topic and save them to your personal
           account.
         </p>
-        <div className="search-form__search-wrapper">
+        <div className='search-form__search-wrapper'>
           <input
-            type="text"
+            type='text'
             required
-            id="news-search-input"
-            name="news-search-input"
-            className="search-form__text-input"
-            placeholder="Nature"
-            minLength="2"
-            maxLength="30"
+            id='news-search-input'
+            name='news-search-input'
+            className='search-form__text-input'
+            placeholder='Nature'
+            minLength={2}
+            maxLength={30}
             value={searchValue}
             onChange={handleInputChange}
           />
           <button
-            type="submit"
-            aria-label="Submit Form"
+            type='submit'
+            aria-label='Submit Form'
             className={`search-form__submit-button ${
               canSearch ? '' : 'search-form__submit-button_disabled'
             }`}
